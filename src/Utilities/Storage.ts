594b11e812e41f87b9dc577aea9ab88cc0bd86ed
Taylor Pridgen <musicinsilence13@gmail.com>
2022-03-11 21:47:50 -0700

    main/ better null checking

--- src/Utilities/Storage.ts
+++ src/Utilities/Storage.ts
@@ -3,7 +3,9 @@ import btoa from "btoa";
 
 export const Storage = {
   get: (key: string) => {
-    return JSON.parse(atob(localStorage.getItem(key) || "") || "");
+    const value = atob(localStorage.getItem(key) || "");
+    if (value) return JSON.parse(value);
+    return undefined;
   },
   set: (key: string, value: any) => {
     localStorage.setItem(key, btoa(JSON.stringify(value)));
